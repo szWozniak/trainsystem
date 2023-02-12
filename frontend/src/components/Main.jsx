27daef88147e82a1126692e01d0985dbd4666dff
szWozniak <szymek.woz@gmail.com>
2023-02-12 03:29:40 +0100

    bootcamp #3 - jwt authorization finished, user profile added

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import Navbar from './Navbar';
 
 import '../styles/Main.scss';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Home from './Home';
 import Login from './Login';
 import Register from './Register';
+import Profile from './Profile';
+import Logout from './Utils/Logout';
 
 const Main = () => {
     return (
         <div className="main">
             <Navbar />
-            <Routes>
+            <Routes> 
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/logout" element={<Logout />} />
                 <Route path="/" element={<Home />} />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </div>
     );
